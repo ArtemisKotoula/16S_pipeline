16bc85bdf1aec3401b4b645829a2ccda01b95832
Artemis Kotoula <170341319+ArtemisKotoula@users.noreply.github.com>
2026-08-12 14:42:06 +0300

    Add 16S analysis pipeline script
    This script executes a 16S analysis pipeline with options for dada2 or kraken modes. It includes quality control, trimming, and analysis steps, logging progress throughout.

---
+++ bin/16S_main.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mode=${1:-}
+
+if [[ -z "$mode" ]]; then
+    echo "Usage: bash 16S_main.sh [dada|kraken]"
+    exit 1
+fi
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${script_dir}/config/config.sh"
+source "${script_dir}/config/conda.sh"
+
+# LOG FUNCTION
+log(){
+    echo "[$(date '+%F %T')] --- $1"
+}
+
+# maybe check for logfile
+mkdir -p "${out_dir}"
+logfile="${out_dir}/pipeline.log"
+
+exec > >(tee -a "$logfile") 2>&1
+
+
+log "Starting 16S pipeline in $mode mode"
+
+
+# common steps
+
+log "Starting QC on raw reads"
+#source "${script_dir}/qc.sh" "${raw_data}" "${fastqc_outDir}/raw" "${multiqc_outDir}/raw" "${threads}"
+#########################
+# TESTING NEW FASTQC FOR PARALLELIZATION
+source "${script_dir}/parallel_qc.sh" "${raw_data}" "${fastqc_outDir}/raw" "${multiqc_outDir}/raw" "${threads}"
+log "Qc for raw reads completed. Results are in ${fastqc_outDir}/raw and ${multiqc_outDir}/raw"
+
+
+log "Starting trimming of primers from raw reads"
+source "${script_dir}/trim.sh" "${raw_data}" "${cutadapt_outDir}" "${r1_primer}" "${r2_primer}" "${threads}" 100
+log "Trimming completed. Results are in ${cutadapt_outDir}"
+
+log "Starting QC on trimmed reads"
+#source "${script_dir}/qc.sh" "${cutadapt_outDir}" "${fastqc_outDir}/trimmed" "${multiqc_outDir}/trimmed" "${threads}"
+#########################
+# TESTING NEW FASTQC FOR PARALLELIZATION
+source "${script_dir}/parallel_qc.sh" "${cutadapt_outDir}" "${fastqc_outDir}/trimmed" "${multiqc_outDir}/trimmed" "${threads}"
+log "Qc for trimmed reads completed. Results are in ${fastqc_outDir}/trimmed and ${multiqc_outDir}/trimmed" 
+
+
+# Choose analysis pipeline
+case "$mode" in
+
+    dada2)
+        bash "${script_dir}/dada2_pipeline.sh"
+        ;;
+
+    kraken)
+        # Last preprocess strep for kraken: quality filtering with fastp
+        log "Starting quality filtering of trimmed reads for Kraken"
+        source "${script_dir}/fastp.sh" "${cutadapt_outDir}" "${fastp_outDir}" "${threads}" "${min_length}" "${quality_threshold}"   
+        log "Quality filtering completed. Results are in ${fastp_outDir}"
+
+        log "Generating fastp summary report"
+        source "${script_dir}/report_fastp.sh" "${fastp_outDir}" "${report_fastp_outDir}"
+        log "fastp summary report generated. Results are in ${report_fastp_outDir}. Samples that do not meet the cutoff criteria have been moved to ${calc_cutoff_outDir}"
+
+        log "Starting QC on filtered reads"
+        source "${script_dir}/parallel_qc.sh" "${fastp_outDir}" "${fastqc_outDir}/filtered" "${multiqc_outDir}/filtered" "${threads}"
+        log "Qc for filtered reads completed. Results are in ${fastqc_outDir}/filtered and ${multiqc_outDir}/filtered"
+
+        log "Starting Kraken-Krona-Bracken analysis"
+        source "${script_dir}/kraken_pipeline.sh" "${fastp_outDir}"
+        log "Kraken-Krona-Bracken analysis and visualization completed. Results are in ${kraken_outDir} and ${phyloseq_outDir}"
+        ;;
+
+    *)
+        echo "Unknown pipeline: ${mode}. Exiting."
+        exit 1
+        ;;
+
+esac
+
+
+
+
+log "Pipeline completed successfully."
